7f4b67968da1b37d30691202ca67fde23fa516a6
Laurynas Jastrižemskis <laurynas.jastrizemskis@stud.svako.lt>
2026-05-19 09:11:10 +0300

    Instaliuotas jetstream + livewire autentifikacijai ir sukurtas finansai puslapis

---
+++ .history/resources/views/finansai.blade_20260519090358.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <div class="p-6">
+        <h1>Finansų programa</h1>
+    </div>
+</x-app-layout>
---
+++ .history/resources/views/finansai.blade_20260519090703.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <div class="p-6" style="margin: 0 auto; width: 200px;">
+        <h1>Finansų programa</h1>
+    </div>
+</x-app-layout>
