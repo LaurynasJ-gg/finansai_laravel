732d9eb44c805fe2a0bfe14ffb87639ce6e978fd
Laurynas Jastrižemskis <laurynas.jastrizemskis@stud.svako.lt>
2026-05-27 09:18:41 +0300

    Pataisytas SuvestineController.php rūšiavimui pagal mėn

--- app/Http/Controllers/SuvestineController.php
+++ app/Http/Controllers/SuvestineController.php
@@ -4,54 +4,74 @@ namespace App\Http\Controllers;
 
 use App\Models\Finansai;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 class SuvestineController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $pajamos = Finansai::where('tipas', 'pajamos')->sum('suma');
+        $menuo = $request->menuo;
+        $query = Finansai::query();
 
-        $islaidos = Finansai::where('tipas', 'islaidos')->sum('suma');
+        if ($menuo) {
+            $query->whereRaw("DATE_FORMAT(data, '%Y-%m') = ?", [$menuo]);
+        }
 
-        $pagalKategorijas = Finansai::select(
-                'kategorijos.pavadinimas',
-                DB::raw('SUM(finansais.suma) as suma')
-            )
+        $pajamos = (clone $query)
+            ->where('tipas', 'pajamos')
+            ->sum('suma');
+
+        $islaidos = (clone $query)
+            ->where('tipas', 'islaidos')
+            ->sum('suma');
+
+        $pagalKategorijas = (clone $query)
+            ->select('kategorijos.pavadinimas', DB::raw('SUM(finansais.suma) as suma'))
             ->join('kategorijos', 'finansais.kategorija_id', '=', 'kategorijos.id')
             ->groupBy('kategorijos.pavadinimas')
             ->get();
 
         $pagalMenesi = Finansai::select(
-            DB::raw("DATE_FORMAT(data, '%Y-%m') as menuo"),
+                DB::raw("DATE_FORMAT(data, '%Y-%m') as menuo"),
 
-            DB::raw("
-                COALESCE(SUM(
-                CASE
-                        WHEN tipas = 'pajamos'
-                        THEN suma
-                        ELSE 0
-                    END
-                ),0) as pajamos
-            "),
+                DB::raw("
+                    COALESCE(SUM(
+                        CASE
+                            WHEN tipas = 'pajamos'
+                            THEN suma
+                            ELSE 0
+                        END
+                    ),0) as pajamos
+                "),
 
-            DB::raw("
-                COALESCE(SUM(
-                    CASE
-                        WHEN tipas = 'islaidos'
-                        THEN suma
-                        ELSE 0
-                    END
-                ),0) as islaidos
-            ")
-        )
+                DB::raw("
+                    COALESCE(SUM(
+                        CASE
+                            WHEN tipas = 'islaidos'
+                            THEN suma
+                            ELSE 0
+                        END
+                    ),0) as islaidos
+                ")
+            )
             ->groupBy('menuo')
             ->orderBy('menuo')
             ->get();
+
+        $menesiList = Finansai::selectRaw("
+                DATE_FORMAT(data, '%Y-%m') as menuo
+            ")
+            ->distinct()
+            ->orderBy('menuo')
+            ->pluck('menuo');
+
         return view('suvestine.index', compact(
             'pajamos',
             'islaidos',
             'pagalKategorijas',
-            'pagalMenesi'
+            'pagalMenesi',
+            'menesiList',
+            'menuo'
         ));
     }
 }
